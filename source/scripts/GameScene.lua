b6bfc3fe9d1cad2e688b3c337cd83ba57a6830c9
Dan Green <danngreen1@gmail.com>
2025-05-18 16:48:44 -0700

    Start FakeWall

--- source/scripts/GameScene.lua
+++ source/scripts/GameScene.lua
@@ -35,8 +35,8 @@ function GameScene:init()
 
 	self.player = Player(self.spawnX, self.spawnY, self)
 
-	self.levelWidth = 800
-	self.levelHeight = 480
+	-- self.levelWidth = 800
+	-- self.levelHeight = 480
 
 	self:cameraFocusAt(self.spawnX, self.spawnY)
 end
@@ -85,7 +85,10 @@ function GameScene:goToLevel(levelName)
 	-- store name for use later
 	self.levelName = levelName
 
-    for layerName, layer in pairs(ldtk.get_layers(levelName)) do
+	local layers = ldtk.get_layers(levelName)
+	assert(layers)
+
+    for layerName, layer in pairs(layers) do
         if layer.tiles then
             local tilemap = ldtk.create_tilemap(levelName, layerName)
 
@@ -112,17 +115,28 @@ function GameScene:goToLevel(levelName)
 
 
 	-- Spawn hazards
-	for _, entity in ipairs(ldtk.get_entities(levelName, layerName)) do
+	local entities = ldtk.get_entities(levelName, layerName)
+	assert(entities)
+
+	for _, entity in ipairs(entities) do
 		local entityX, entityY = entity.position.x, entity.position.y
 		local entityName = entity.name
+
 		if entityName == "Spike" then
 			Spike(entityX, entityY)
+
 		elseif entityName == "Ability" then
 			Ability(entityX, entityY, entity)
+
 		elseif entityName == "SlidingDoor" then
 			SlidingDoor(entityX, entityY)
+
 		elseif entityName == "Coin" then
 			Coin(entityX, entityY, entity)
+
+		elseif entityName == "FakeWall" then
+			FakeWall(entityX, entityY, entity)
+
 
 		end
 	end
